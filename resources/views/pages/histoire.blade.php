fb296f1c7da86b585ec50633829b686beaeb357b
Antoy Abdallah <antoyabdallah@gmail.com>
2019-08-11 14:38:27 +0200

    Correction fichier histoire

---
+++ resources/views/pages/histoire.blade.php
@@ -0,0 +1,228 @@
+@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <!-- ===============================================-->
+    <!--    Document Title-->
+    <!-- ===============================================-->
+    <title>Une histoire Mahoraise</title>
+
+
+    <!-- ===============================================-->
+    <!--    Favicons-->
+    <!-- ===============================================-->
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicons/favicon-16x16.png">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicons/favicon.ico">
+    <link rel="manifest" href="assets/img/favicons/manifest.json">
+    <meta name="msapplication-TileImage" content="assets/img/favicons/mstile-150x150.png">
+    <meta name="theme-color" content="#ffffff">
+
+
+    <!-- ===============================================-->
+    <!--    Stylesheets-->
+    <!-- ===============================================-->
+    <link href="https://fonts.googleapis.com/css?family=Merriweather:300,400,700,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">
+    <link href="assets/lib/loaders.css/loaders.min.css" rel="stylesheet">
+    <link href="assets/lib/owl-carousel/owl.carousel.min.css" rel="stylesheet">
+    <link href="assets/lib/fancy-tab/fancy-tab.css" rel="stylesheet">
+    <link href="assets/css/theme.css" rel="stylesheet">
+    <link href="assets/css/myStyle.css" rel="stylesheet">
+
+  </head>
+
+
+  <body>
+
+    <!-- ===============================================-->
+    <!--    Main Content-->
+    <!-- ===============================================-->
+    <main class="main" id="top">
+      <!-- ============================================-->
+      <!-- Preloader ==================================-->
+      <div class="preloader p-0" id="preloader">
+        <div class="loader d-flex flex-column align-items-center"><img src="assets/img/illustrations/egg.gif" alt="" /></div>
+      </div>
+      <!-- ============================================-->
+      <!-- End of Preloader ===========================--><a class="bottom-to-top rounded-soft" href="#top" data-fancyscroll><span class="fas fa-angle-up lg" data-fa-transform="down-2"></span></a>
+      <!--+navbar.navbar-expand-md.navbar-dark.bg-dark.sticky-top.font-weight-semi-bold.fs--1-->
+      
+
+    
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+      <section class="pt-7 pb-4">
+
+        <div class="bg-holder" style="background-image:url(assets/img/gallery/zainoussa.jpg);">
+        </div>
+        <!--/.bg-holder-->
+
+        <div class="container">
+          <div class="row min-vh-25 align-items-center text-center justify-content-center">
+            <div class="col col-md-7">
+              <h1 class="text-primary fs-2 fs-md-5">Une Histoire Mahoraise..</h1>
+              <div class="lead text-white text-uppercase fs-0 fs-md-1">Découvrez la petite et la grande histoire de cette petite île de l'Océan Indien.</div>
+            </div>
+          </div>
+        </div>
+        <!-- end of .container-->
+
+      </section>
+      <!-- <section> close ============================-->
+      <!-- ============================================-->
+
+      <section class="py-0" style="margin-top: -300px;">
+        <div class="bg-holder d-none d-lg-block" style="background-image:url(assets/img/food/.png);background-position:left;background-size:35%; transform:none;" data-zanim-xs='{"from":{"x":"-20%","opacity":0},"to":{"x":"0%","opacity":1},"delay":0,"ease":"Power0.easeOut"}'>
+        </div>
+        <!--/.bg-holder-->
+
+        <div class="row align-items-md-center min-vh-100 no-gutters overflow-hidden" data-zanim-timeline="{&quot;delay&quot;:0}" data-zanim-trigger="scroll">
+          <div class="col-md-7 col-lg-6 col-xl-5 col-xxl-4 mt-6 mt-md-0 text-center text-md-left offset-lg-1 order-md-1 px-3 pt-6">
+            <div class="overflow-hidden">
+              <h1 class="font-weight-light fs-xl-4 fs-sm-3 fs-1" data-zanim-xs='{"animation":"default","delay":0.5,"duration":1.5}'>Mayotte,<span class="font-weight-black">toute une histoire<br/>Qui attend d'être  </span>connue.</h1>
+            </div>
+            <div class="overflow-hidden">
+              <p class=" myHeader d-none d-md-block" data-zanim-xs='{"animation":"default","delay":0.6,"duration":1.5}'>Pour savoir où on va, il vaut mieux ne pas oublier d'où l'ont vient. . 
+La culture mahoraise a l'avantage d'être encore très préservée et pas encore diluée dans la mondialisation, quoiqu'on peut sentir le périle imminet. Il faut veiller à la conserver et à la developper autant que possible. La jeunesse semble se perdre entre un mode de vie à l'occidental qui les fait rêver sans toutefois en mesurer tous les inconveinients. Ainsi hélas, on expérimente beaucoup des inconveinients, tels les drogues, l'alcool, la polution due à une consommation mal adaptée ou l'individualisme.. loin d'imaginer que bon nombre des solutions pour le salut de l'île passent par les aînés et ce qu'ils ont à nous enseigner de leur vie d'avant, qui en dépit d'une grande pauvreté, connaissaient l'entre aide, la solidarité, et l'esprit de la communauté.</p>
+            </div>
+           
+          </div>
+          <div class="col-md col-lg-4 mt-4 mt-sm-0 overflow-hidden">
+            <div class="ml-md-6" data-zanim-xs='{"from":{"x":"-100%","opacity":0},"to":{"x":"0%","opacity":1},"delay":0.5,"ease":"Power0.easeOut"}'><img class="img-fluid" src="assets/img/illustrations/histoire.png" alt="" data-zanim-xs='{"from":{"rotation":0},"to":{"rotation":60},"ease":"Power0.easeOut","delay":0.5}' /></div>
+          </div>
+        </div>
+      </section>
+
+
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+      <section id="menu" class="pushUp">
+
+        <div class="container">
+          <div class="row mb-6" data-zanim-timeline="{&quot;delay&quot;:0}" data-zanim-trigger="scroll">
+            <div class="col text-center">
+              <div class="overflow-hidden">
+                <p class="fs-2 text-1000 mb-1 font-weight-bold" data-zanim-xs='{"animation":"","delay":0,"duration":1}'>Histoire</p>
+                <h2 class="fs-3 fs-sm-4 font-weight-bold" data-zanim-xs='{"animation":"","delay":0.1,"duration":1}'>Apprendre un peu plus sur l'histoire de Mayotte</h2>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <div class="owl-carousel owl-theme owl-hvr-nav" data-options='{"margin":10,"autoplay":true,"dots":false,"loop":true,"responsive":{"0":{"items":1,"nav":false},"767":{"items":2,"nav":true},"992":{"items":4,"nav":true}}}'>
+
+                <div class="hoverbox"><img class="rounded-soft" src="assets/img/illustrations/histoire.jpg" alt=" " />
+                  <div class="hoverbox-content hoverbox-content-gradient justify-content-center align-items-center rounded-soft">
+                    <div class="lh-3">
+                      <p class="font-weight-bold fs-2 mb-0 text-uppercase text-white mt-10 lh-3">Spicy Chicken <br> spaghetti</p>
+                      <p class="text-white">Served with green chillies</p>
+                    </div>
+                  </div>
+                </div>
+
+                
+               <a class="text-decoration-none" href="#" data-toggle="modal" data-target="#boueniZaina">
+                  <div class="hoverbox"><img class="rounded-soft" src="assets/img/illustrations/zaina.jpg" alt=" " />
+                  <div class="hoverbox-content hoverbox-content-gradient justify-content-center align-items-center rounded-soft">
+                    <div class="lh-3">
+                      <p class="font-weight-bold fs-2 mb-0 text-uppercase text-white mt-10 lh-3">Spicy Chicken <br> spaghetti</p>
+                      <p class="text-white">Served with green chillies</p>
+                    </div>
+                  </div>
+                </div>
+              </a>
+                  
+            </div>
+          </div>
+        </div>
+        <!-- end of .container-->
+
+      </section>
+
+
+            <div  class="modal fade" id="boueniZaina" tabindex="-1" role="dialog" aria-hidden="true">
+        <div style="min-width: 80% !important ;right: 10%; box-sizing: border-box; right: 10%: height:95vh; bottom: 10%; top: 10px !important; position: absolute;" class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content border-0">
+            <div class="modal-body">
+              <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+              <div class="row">
+                <div class="border border-primary border-2x ml-5 rounded-soft"></div>
+                <div class="col-10">
+                  <p class="fs-2 font-weight-bold text-uppercase lh-3 mt-3">Bouéni Zaina</p>
+                </div>
+              </div>
+              <div class="row mt-4">
+                <div class="col-md-8 col-7 pl-0"><img class="img-fluid illustrationArticle" src="assets/img/illustrations/zena2.jpg" alt="" /></div>
+                <div class="col-md-4 col-5 border-left border-200 border-2x h-50">
+                  <ul class="list-unstyled text-500">
+                    <li>Histoire</li>
+                  </ul>
+                </div>
+              </div>
+              <div class="row justify-content-center bg-light">
+                <div class="col-10 pt-3">
+                  <p> S'il est une chose dont les mahorais sont fiers, c'est de leur appartenance à la France. Les mahorais ont du se battre jusqu'à une periode encore très récente pour montrer leur désir d'être Français et obtenir leur integration pleine et entière à la France puisque Mayotte est devenue le 101ème département français en mars 2011, promesse faite par Mitterand sans cesse reportée et concrétisée finalment par l'ancien président de la République Française Nicolas Sarkozy, mettant ainsi fin à des décénnies de promesses que ses prédécesseurs n'avaient pas tenues...</p>
+                </div>
+              </div>
+              <div class="modal-footer justify-content-center">
+                <a href="post/show/3"><button class="btn btn-primary btn-block text-center" type="button">Lire +</button></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+      
+
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+      <section class=" myFooter bg-black py-4 text-center">
+
+        <div class="container">
+          <p class="mb-0 font-weight-bold d-inline-block">Vivez Mayotte différemment..<span class="text-primary fs-0">&hearts;</span> avec </p><a class="text-white ml-2" href="https://themewagon.com/">Hello Mayotte</a>
+        </div>
+        <!-- end of .container-->
+
+      </section>
+      <!-- <section> close ============================-->
+      <!-- ============================================-->
+
+      <!-- ============================================-->
+
+
+    </main>
+    <!-- ===============================================-->
+    <!--    End of Main Content-->
+    <!-- ===============================================-->
+
+
+
+
+    <!-- ===============================================-->
+    <!--    JavaScripts-->
+    <!-- ===============================================-->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/plugins.js"></script>
+    <script src="assets/lib/is/is.min.js"></script>
+    <script src="assets/lib/stickyfilljs/stickyfill.min.js"></script>
+    <script src="assets/lib/sticky-kit/sticky-kit.min.js"></script>
+    <script src="assets/lib/loaders.css/loaders.css.js"></script>
+    <script src="assets/lib/owl-carousel/owl.carousel.min.js"></script>
+    <script src="assets/lib/fancy-tab/fancy-tab.min.js"></script>
+    <script src="assets/js/theme.js"></script>
+
+  </body>
+
+</html>
